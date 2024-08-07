9165b266ca411794cf5bfc290b595611c9120a11
R-Paskvan <rpaskvan@umich.edu>
2024-08-07 18:42:40 -0400

    Adding CombineSentiment and Hot Encoding

---
+++ CombineSentiment.py
@@ -0,0 +1,3 @@
+# This function works to split, apply, combine on a dataframe
+# from the CalcOneHotEncode function. It is grouped by days
+# and the vectors of sentiment and scaled by score are added
