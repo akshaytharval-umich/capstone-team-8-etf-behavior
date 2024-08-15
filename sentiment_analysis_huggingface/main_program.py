d44f9dba2f5aee3e26716418c85ce7721be25bd8
reipask <rpaskvan@umich.edu>
2024-08-15 18:30:12 -0400

    Update to main program

--- sentiment_analysis_huggingface/main_program.py
+++ sentiment_analysis_huggingface/main_program.py
@@ -12,31 +12,31 @@ column_names = ['abstract','lead_paragraph','full_text']
 
 # Sentiment Analysis Stage
 # First load in articles.csv "full_text"
-#df = pd.read_csv("csentiment_analysis_huggingface/omplete_raw_articles.csv",encoding='utf-8',index_col=False)
-#for column_name in column_names:
-#    for model_name in model_names:
-#        df = analyze_sentiment(df,column_name,model_name)
+df = pd.read_csv("csentiment_analysis_huggingface/omplete_raw_articles.csv",encoding='utf-8',index_col=False)
+for column_name in column_names:
+    for model_name in model_names:
+        df = analyze_sentiment(df,column_name,model_name)
 #then save the df to csv
-#df.to_csv("sentiment_analysis_huggingface/articles_sentiment.csv",encoding='utf-8',index=False)
+df.to_csv("sentiment_analysis_huggingface/articles_sentiment.csv",encoding='utf-8',index=False)
 
-#df = pd.read_csv("sentiment_analysis_huggingface/articles_sentiment.csv",encoding='utf-8',index_col=False)
-#column_lst = utils.determine_col_lst(df)
+df = pd.read_csv("sentiment_analysis_huggingface/articles_sentiment.csv",encoding='utf-8',index_col=False)
+column_lst = utils.determine_col_lst(df)
 
-#encoded_df = utils.calc_one_hot_encode(df,column_lst)
-#encoded_df.to_csv("sentiment_analysis_huggingface/articles_encoded.csv",encoding='utf-8',index=False)
+encoded_df = utils.calc_one_hot_encode(df,column_lst)
+encoded_df.to_csv("sentiment_analysis_huggingface/articles_encoded.csv",encoding='utf-8',index=False)
 
 # Next pre-processing before grouping
-#df = pd.read_csv("sentiment_analysis_huggingface/articles_encoded.csv",encoding='utf-8',index_col=False)
-#new_df = utils.scale_vectors(df)
-#new_df.to_csv("sentiment_analysis_huggingface/articles_scaled.csv",encoding='utf-8',index=False)
+df = pd.read_csv("sentiment_analysis_huggingface/articles_encoded.csv",encoding='utf-8',index_col=False)
+new_df = utils.scale_vectors(df)
+new_df.to_csv("sentiment_analysis_huggingface/articles_scaled.csv",encoding='utf-8',index=False)
 
 # Next timeshift the weekends
-#df = utils.time_shift("sentiment_analysis_huggingface/articles_scaled.csv")
-#df.to_csv("sentiment_analysis_huggingface/articles_shifted.csv",encoding='utf-8',index=True)
+df = utils.time_shift("sentiment_analysis_huggingface/articles_scaled.csv")
+df.to_csv("sentiment_analysis_huggingface/articles_shifted.csv",encoding='utf-8',index=True)
 
-#df = pd.read_csv("sentiment_analysis_huggingface/articles_shifted.csv",encoding='utf-8',index_col=False)
-#df = utils.group_apply_reduce(df)
-#df.to_csv("sentiment_analysis_huggingface/articles_grouped.csv",encoding='utf-8',index=True)
+df = pd.read_csv("sentiment_analysis_huggingface/articles_shifted.csv",encoding='utf-8',index_col=False)
+df = utils.group_apply_reduce(df)
+df.to_csv("sentiment_analysis_huggingface/articles_grouped.csv",encoding='utf-8',index=True)
 
 df = pd.read_csv("sentiment_analysis_huggingface/articles_grouped.csv",encoding='utf-8',index_col='pub_date')
 voo = pd.read_csv("sentiment_analysis_huggingface/VOO_historical_data.csv",encoding='utf-8',index_col='Date')
